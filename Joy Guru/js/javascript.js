53d59972e1571eb81887e4accf7fb94af6151d30
Sabarnna Sen <sabarnna130@gmail.com>
2021-10-28 19:59:39 +0530

    Delete Joy Guru directory

--- Joy Guru/js/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-                $('#message').waypoint(function(direction) {
-                    $('#message').addClass("animate__animated animate__fadeInUp ")
-                    console.log('I am 20px from the top of the window')
-                }, {
-                    offset: '75%'
-                })
-            });
